8bbb30055d3979392e5ac134d1fdc04c6336ce02
Guilherme de Castro <guilherme@decastro.me>
2026-09-08 21:10:21 -0300

    style(icons): hide page header separator

--- ui/internal/view/icons_templ.go
+++ ui/internal/view/icons_templ.go
@@ -90,7 +90,7 @@ func IconsContent(data IconPage) templ.Component {
 			templ_7745c5c3_Var3 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = pageheader.PageHeader(pageheader.Config{Title: "Icons", Actions: IconPageActions(data)}).Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = pageheader.PageHeader(pageheader.Config{Title: "Icons", HideSeparator: true, Actions: IconPageActions(data)}).Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
